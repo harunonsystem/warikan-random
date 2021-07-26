8b10e3dc761cd2a59622a0f429f01b3b301482c9
harunon <harunonsystem@gmail.com>
2021-07-26 20:30:42 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script lang="tsx">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import logo from './assets/logo.png'
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return () => (
+      <>
+        <img alt="Vue logo" src={logo} />
+        <HelloWorld msg="warikan-random" /> 
+      </>
+    )
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
